075c4e54e97135fecff2588bfb8d3cdb3c01066f
Joseph Fuentes <106282075+Josephuentes@users.noreply.github.com>
2022-07-09 22:28:55 -0700

    Add files via upload

---
+++ WARtest.js
@@ -0,0 +1,15 @@
+
+const expect = chai.expect
+
+
+describe ('createDeck', function (){
+    describe('#createDeck', function (){
+        it ('should have 52 elements in the array', function() {
+            var suits = ['H', 'C', 'D', 'S']
+    var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    var deck = []; 
+    expect(createDeck(deck.length).to.deep.equal(52))
+        })
+    })
+})
+
